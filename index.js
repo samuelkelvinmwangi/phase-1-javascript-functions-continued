081b325701f0517262311ba9b39b73e0375e4eca
samuel <samuelkelvinmwangi@gmail.com>
2023-03-29 17:17:38 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(idea){
+    if(idea == 'bathe my dog'){
+            return("This Saturday, I want to bathe my dog!");
+    }
+
+    return ("This Saturday, I want to roller-skate!");
+}
+
+function mondayWork(work){
+    if(work == "work from home"){
+        return ("This Monday, I will work from home.");
+    }
+    return ("This Monday, I will go to the office.");
+}
